72359c96336a5c3839e4690cbf12e817bc4c8782
neal03shah <91294138+neal03shah@users.noreply.github.com>
2021-10-06 19:07:08 -0700

    Added some programs from Session 3.
    Bring the func to the function.

--- functions.py
+++ functions.py
@@ -2,25 +2,26 @@ import numpy as np
 import sys
 
 #define a function that returns a value
-def expo(x):
-	return np.exp(x)	#return the np e^x function
+
+def expo(x) :
+	return np.exp(x)	#return e^x 
 
 #define a subroutine that does not return a value
 def show_expo(n):
+	for i in range(n):
+		print(expo(float(i)))	#call expo
 
-	for i in range(n): #i = [0, n-1]
-		print(expo(float(i)))
-
-#define main function
+#define a main function
 def main():
-	n = 10 #provide a default value for n
+	n = 10 #default value for n
 
-	#check if there is a command line argument provided
+	#check if there is a command line
+	#argument that has been passed?
 	if(len(sys.argv)>1):
-		n = int(sys.argv[1]) #change n to be the 2nd command line arg
+		n - int(sys.argv[1])	#reset n
+
 	show_expo(n)
 
 #run the main function
 if __name__=="__main__":
 	main()
-
